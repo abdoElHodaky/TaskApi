3177b12f0c677084156cf4e7130adc55999971f5
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-01-22 16:40:20 +0200

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,5 @@
 #/usr/bin/bash
 
-composer require --dev mpociot/laravel-apidoc-generator@4.8.2
+composer require --dev mpociot/laravel-apidoc-generator
 php artisan vendor:publish --provider="Mpociot\ApiDoc\ApiDocGeneratorServiceProvider" --tag=apidoc-config
 php artisan apidoc:generate
