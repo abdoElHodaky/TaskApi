6462d2469c509907d2ab928e41cbf9cbe3ce318b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-01-22 17:05:56 +0200

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,5 @@
 #/usr/bin/bash
 
-composer require rakutentech/laravel-request-docs:2.28
+#composer require rakutentech/laravel-request-docs:2.28
 php artisan vendor:publish --tag=request-docs-config && php artisan route:cache
 
