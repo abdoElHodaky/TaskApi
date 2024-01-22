ae96345dfee1ed9d9594963502bcc22d9069e816
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-01-22 14:52:24 +0200

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,6 @@
 #/usr/bin/bash
 
-composer install
+composer update
 php artisan db:wipe --drop-types --force && php artisan migrate:install
 php artisan migrate --force
 php artisan db:seed --force
