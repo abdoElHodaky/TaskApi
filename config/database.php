c368565d30a82b415ad05274521fbe00ad5a1566
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-01-22 14:16:09 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DATABASE_URL',"postgres://jvvafpjz:OhYPth-rIVlpe03a_lGXuW7jVprYrnWI@hattie.db.elephantsql.com/jvvafpjz"),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
