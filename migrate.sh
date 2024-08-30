45d77669264979cfd01e7ec52dfc9932ca9043e8
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-08-30 18:38:04 +0300

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,6 @@
 #/usr/bin/bash
 
-composer require spatie/laravel-responsecache
-php artisan vendor:publish --tag="responsecache-config"
+#composer require spatie/laravel-responsecache
+#php artisan vendor:publish --tag="responsecache-config"
 php artisan vendor:publish --tag=request-docs-config && php artisan route:cache
 
